9381fd64f0e66d6ff090c7998c4060d02e636a89
Saurabh Saxena <saurabh.jan03@gmail.com>
2023-10-18 23:34:52 -0400

    Initial setup commit

--- src/main/java/com/example/azure/devops/controller/SimpleController.java
+++ src/main/java/com/example/azure/devops/controller/SimpleController.java
@@ -1,3 +1,8 @@
+package com.example.azure.devops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class SimpleController {
 
